fabe6cb2720c9c674e8e4694e11fd7f5a2ca1478
Felg-Ts <el-feli-rubio1996@hotmail.com>
2021-03-06 17:34:43 +0100

    Create funciones.py
    Añado el fichero de funciones con la primera función creada.

---
+++ funciones.py
@@ -0,0 +1,16 @@
+import msvcrt
+import os
+
+def listinfo(chfile):
+    for i in chfile['Champion']:
+        print('Champion-name:' ,i['name'])
+        print('Habilidad-pasiva:' ,i['abilities']['Ability'][0]['name'])
+        print('Habilidad-1:' ,i['abilities']['Ability'][1]['name'])
+        print('Habilidad-2:' ,i['abilities']['Ability'][2]['name'])
+        print('Habilidad-3:' ,i['abilities']['Ability'][3]['name'])
+        print('Habilidad-4:' ,i['abilities']['Ability'][4]['name'])
+        print('')
+    
+    print("Presione una tecla para continuar...")
+    msvcrt.getch()
+    os.system('cls')
